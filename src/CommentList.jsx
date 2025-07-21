302f8ec5d07d8164c6ba8b917c6fe0758ae6f213
Bahir Azizi <bahirazizi7@gmail.com>
2025-07-21 11:32:59 +0430

    Status functionality added to the user side

--- src/CommentList.jsx
+++ src/CommentList.jsx
@@ -1,16 +1,36 @@
 import axios from "axios";
 
 function CommentList({ comments }) {
-  console.log("commnet portion",comments);
+
+  
+  const renderComments = comments.map((comment) => {
+    let content;
+    let color
+  switch (comment.status) {
+    case 'approved':
+      content = comment.content;
+      color = 'green';
+      break;
+    case 'pending':
+      content = 'This comment is awaiting moderation';
+      color = 'orange';
+      break;
+    case 'rejected':
+      content = 'This comment has been rejected';
+      color = 'red';
+      break;
+    default:
+      content = 'Unknown status';
+      color = 'gray';
+  }
+    return <li key={comment.id} style={{color:color}} >{content}</li>;
+
+  })
   return (
-    <ul>
-      {comments.map(comment => (
-        <li key={comment.id}>
-          <strong>{comment.content}</strong>
-        </li>   
-      ))}
-    </ul>
+    <ul>{renderComments} </ul>
   );
+
+
 }
 
 export default CommentList;
